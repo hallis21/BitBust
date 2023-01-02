25350b8d0d7c5bf191f84b1f22ae0679c88af35b
hallis21 <halvor_ring@hotmail.com>
2023-01-02 02:11:59 +0100

    hmmm

--- Buster.py
+++ Buster.py
@@ -1,156 +1,358 @@
 import asyncio
+import os
 from time import sleep
 import time
-from typing import List
+from ActionClasses import SingleAction, CompoundAction
+import win32api
+import pyautogui
+import rotatescreen
+import win32con
+import LocationValues
+
+
 
 # State variables
-
-running = True
-active = False
-
-ensure_inventory_open = False
-ensure_inventory_close = False
-
-inventory_is_open = False
-tarkov_is_active = False
-
-action_lock = asyncio.Lock()
-action_queue = []
-
-
-# Create a lock for writing to file
-write_lock = asyncio.Lock()
-
-async def write_to_file(data: str):
-    # Append timestamp to log
-    data = f"{int(time.time())} {data}"
-    async with write_lock:
-        with open("log.txt", "a") as f:
-            f.write(data)
-            print(data, end="\n" if data[-1] != "\n" else "")
-            if data[-1] != "\n":
-                f.write("\n")
-
-
-# Defines an action
-# The action can be compund, if they are they will be executed at the same time
-# Example walk forward while shooting
-class SingleAction:
-    def __init__(self, action, args):
-        self.action = action
-        self.args = args
+class Buster:
+    
+    # Name:function
+    
+    def __init__(self):
         
-    async def execute(self):
-        if tarkov_is_active:
-            await self.action(*self.args)
-
-
-class CompoundAction:
-    def __init__(self, actions: List[SingleAction]):
-        self.actions = actions
+        self.rewards = {
+            "open_inventory": self.open_inventory, # Will ensure inv in this state 
+            "close_inventory": self.close_inventory, # Will ensure inv in this state
+            "drop_primary": self.drop_primary,
+            "drop_secondary": self.drop_secondary,
+            "drop_pistol": self.drop_pistol,
+            "drop_armor": self.drop_armor,
+            "drop_all_weapons": self.drop_all_weapons,
+            "drop_all_wearable": self.drop_all_wearable,
+            "drop_rig":self.drop_rig,
+            "drop_backpack": self.drop_backpack
+        }
         
-    async def execute(self):
-        to_await = []
-        for action in self.actions:
-            # Do not await, we want to execute all at the same time
-                to_await.append(action.execute())
-        await asyncio.gather(*to_await)
+        self.running = True
+        self.active = False
+
+        self.ensure_inventory_open = False
+        self.ensure_inventory_close = False
+
+        self.in_raid = False
+        self.inventory_is_open = False
+        self.tarkov_is_active = False
+
+        self.action_lock = asyncio.Lock()
+        self.action_queue = []
 
 
+        self.write_lock = asyncio.Lock()
 
 
+    async def parse_action(self, action: str):
+        if action in self.rewards:
+            # Create single action
+            a = SingleAction(self.rewards[action], [], self)
+            print("Queing action", action)
+            await self.queue_action(a)
+        else:
+            await self.write_to_file(f"Action {action} does not exist")
 
-# These will run on an interval to constantly check the states
-# will update the variables above
-async def tarkov_check():
-    # TODO
-    return True
-async def inventory_check():
-    # TODO
-    return False
+    async def write_to_file(self, data: str):
+        # Append timestamp to log
+        data = f"{int(time.time())} {data}"
+        async with self.write_lock:
+            # if file no exist, create it
+            if not os.path.exists("log.txt"):
+                with open("log.txt", "w+") as f:
+                    f.write("")
+            with open("log.txt", "a") as f:
+                f.write(data)
+                print(data, end="\n" if data[-1] != "\n" else "")
+                if data[-1] != "\n":
+                    f.write("\n")
+
+
+    async def rotate_screen(self, duration: float = 1):
+        screen = rotatescreen.get_primary_display()
+        current_orientation = screen.current_orientation
+        try:
+            if current_orientation == 180:
+                screen.rotate_to(0)
+            elif current_orientation == 0:
+                screen.rotate_to(180)
+            await asyncio.sleep(duration)
+        finally:
+            screen.rotate_to(current_orientation)
+
+
+    # Will queue an action to be executed when tarkov is the active window
+    # do not run multiple of these at the same time
+    async def queue_action(self, action):
+        async with self.action_lock:
+            print("Queued action")
+            self.action_queue.append(action)
+                
+    async def drop_item(self, item):
+        tries = 0
+        while not self.inventory_is_open:
+            await asyncio.sleep(0.05)
+            tries += 1
+            if tries > 20:
+                await self.write_to_file(f"Failed to drop item {item} because inventory was not open")
+                return False
+            
+        try:
+            dropped = False
+            while not dropped: 
+                if not self.ensure_inventory_open: raise Exception("Inventory is not ensured") 
+                if not self.inventory_is_open: continue
+                loc = LocationValues.SLOT_ABSOLUTE_POSITIONS[item]
+                pyautogui.moveTo(loc[0], loc[1], 0, pyautogui.easeInOutQuad)
+                # use clip cursor to lock the mouse to a certain area
+                win32api.ClipCursor((loc[0], loc[1], loc[0], loc[1]))
+                
+                pyautogui.press('delete')
+                # wait since we want to check if inventory was closed
+                await asyncio.sleep(0.05)
+                if not self.inventory_is_open: continue
+                dropped = True
+            # Realease the cursor
+        except Exception as e:
+            await self.write_to_file(f"Failed to drop item {item} because {e}")
+            return False
+        finally:
+            await self.write_to_file(f"Dropped item {item}")
+            win32api.ClipCursor((0,0,0,0))
+        return True        
+            
+    
+    async def scroll_inventory(self):
+        tries = 0
+        while not self.inventory_is_open:
+            await asyncio.sleep(0.05)
+            tries += 1
+            if tries > 20:
+                await self.write_to_file(f"Failed to scroll inventory because inventory was not open")
+                return False
+        
+        loc = LocationValues.SLOT_ABSOLUTE_POSITIONS["INVENTORY"]
+        pyautogui.moveTo(loc[0], loc[1], 0, pyautogui.easeInOutQuad)
+
+        for _ in range(10):
+            win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, 25, 0)
+            await asyncio.sleep(0.005)
+
+    # Will drop one of the main items, weapons, armor, etc
+    async def drop_main_item(self, item):
+        # await self.drop_item(item)
+        pass
+
+
+    async def drop_primary(self):
+        self.ensure_inventory_open = True
+        print(f"Dropping primary {self.inventory_is_open} {self.ensure_inventory_open}")
+        await self.drop_item("PRIMARY")
+        self.ensure_inventory_open = False
+        await self.close_inventory()
+
+    async def drop_secondary(self):
+        self.ensure_inventory_open = True
+        await self.drop_item("SECONDARY")
+        self.ensure_inventory_open = False
+        await self.close_inventory()
+
+    async def drop_pistol(self):
+        self.ensure_inventory_open = True
+        await self.drop_item("PISTOL")
+        self.ensure_inventory_open = False
+        await self.close_inventory()
+
+    async def drop_armor(self):
+        self.ensure_inventory_open = True
+        await self.drop_item("ARMOR")
+        self.ensure_inventory_open = False
+        await self.close_inventory()
+        
+    async def drop_all_weapons(self):
+        self.ensure_inventory_open = True
+        await self.drop_item("PISTOL")
+        await self.drop_item("SECONDARY")
+        await self.drop_item("PRIMARY")
+        self.ensure_inventory_open = False
+        await self.close_inventory()
+        await asyncio.sleep(0.5)
         
 
-# Will queue an action to be executed when tarkov is the active window
-# do not run multiple of these at the same time
-async def queue_action(action):
-    async with action_lock:
-        action_queue.append(action)
-        print("Queued action")
+        
+        
+    
+    async def drop_rig(self):
+        self.ensure_inventory_open = True
+        await self.scroll_inventory()
+        await self.drop_item("RIG")
+        self.ensure_inventory_open = False
+        await self.close_inventory()
+
+    async def drop_backpack(self):
+        self.ensure_inventory_open = True
+        await self.scroll_inventory()
+        await self.drop_item("BACKPACK")
+        self.ensure_inventory_open = False
+        await self.close_inventory()
+        
+        
+    async def drop_all_wearable(self):
+        # win32api press v
+        win32api.keybd_event(0x56, 0, 0, 0)
+        await asyncio.sleep(0.5)
+        
+        self.ensure_inventory_open = True
+        
+        await self.drop_item("ARMOR")
+        await self.drop_item("HEADWEAR")
+        await self.drop_item("FACECOVER")
+        await self.drop_item("HEADPHONE")
+        await self.scroll_inventory()
+        await self.drop_item("RIG")
+        await self.drop_item("BACKPACK")
+        self.ensure_inventory_open = False
+        await self.close_inventory()
+        await asyncio.sleep(0.7)
+        
         
 
-# Will drop one of the main items, weapons, armor, etc
-async def drop_main_item(item):
-    pass
+    async def press_key(self, key, duration):
+        print(f"Pressing {key} for {duration} seconds")
+        await asyncio.sleep(duration)
+        print(f"Pressed {key} for {duration} seconds")
+
+            
+
+    async def open_inventory(self):
+        # Ensure inventory is open for 1 second
+        self.ensure_inventory_open = True
+        await asyncio.sleep(1)
+        self.ensure_inventory_open = False
 
 
-async def drop_primary():
-    pass
-
-async def drop_secondary():
-    pass
-
-
-async def drop_pistol():
-    pass
-
-async def drop_armor():
-    pass
-
-async def press_key(key, duration):
-    await asyncio.sleep(duration)
-    print(f"Pressed {key} for {duration} seconds")
-
-async def open_inventory():
-    pass
-
-
-async def close_inventory():
-    pass
-
-
-# Name:function
-rewards = {
-    "open_inventory": open_inventory, # Will ensure inv in this state 
-    "close_inventory": close_inventory, # Will ensure inv in this state
-    "primary": drop_primary,
-    "secndary": drop_secondary,
-    "pistol": drop_pistol,
-    "armor": drop_armor
-}
+    async def close_inventory(self):
+        if self.inventory_is_open:
+            pyautogui.press('tab')
 
 
 
-async def action_executor_task():
-    print("lol")
-    while running:
-        if not active:
-            await asyncio.sleep(0.5)
-            continue
-        async with action_lock:
-            if len(action_queue) > 0:
-                action = action_queue.pop(0)
-                await action.execute()
+
+
+    async def main_loop(self):
+        last_action = None
+        while self.running:
+            try:
+                if not self.active:
+                    await asyncio.sleep(0.5)
+                    continue
+                
+                action = None
+                async with self.action_lock:
+                    if len(self.action_queue) > 0 and (not last_action or last_action.done()):
+                        action = self.action_queue.pop(0)
+                
+                if action: 
+                    new_loop = asyncio.get_event_loop()
+                    last_action = new_loop.create_task(action.execute())
+                await asyncio.sleep(0.1)
+            finally:
+                # Log the current states
+                pass
+                # await self.write_to_file(f" Active: {self.active}, Tarkov Active: {self.tarkov_is_active}, Inventory: {self.inventory_is_open}, Ensure Open: {self.ensure_inventory_open}")
+                # print(f"Active: {self.active}, Tarkov Active: {self.tarkov_is_active}, Inventory: {self.inventory_is_open}, Ensure Open: {self.ensure_inventory_open}", end="\r")
+                
+                
+                
+    async def check_tarkov_task(self):
+        while self.running:
+            self.tarkov_is_active = pyautogui.getActiveWindowTitle() == "EscapeFromTarkov"
             await asyncio.sleep(0.01)
+            
+    async def check_in_raid_task(self):
+        # self.in_raid = True
+        # return
+        while self.running:
+            if self.tarkov_is_active:
+                self.in_raid = not (bool(pyautogui.locateOnScreen('slots/settings.png', region=LocationValues.SETTINGS, confidence=0.7)) or\
+                    bool(pyautogui.locateOnScreen('slots/main_menu.png', region=LocationValues.MAIN_MENU, confidence=0.7)))
+            else:
+                self.in_raid = False
+            await asyncio.sleep(0.5)
+            
+            
+    async def check_inv_task(self):
+        last = time.time()
+        while self.running:
+            cur = time.time()
+            if cur - last < 0.05:  
+                await asyncio.sleep(0.01 - (cur - last))
+            if not self.tarkov_is_active:
+                self.inventory_is_open = False
+                await asyncio.sleep(0.1)
+            else:
+                self.inventory_is_open = \
+                    bool(pyautogui.locateOnScreen('slots/INV_CHECK.png', 
+                        region=LocationValues.INVENTORY_CHECK_LOC,
+                        confidence=0.7))
+                    
+                # Ensure inventory is in the correct state
+                if self.tarkov_is_active and self.ensure_inventory_open and not self.inventory_is_open:
+                    print("Opening inventory")
+                    pyautogui.press('tab')
+                    await asyncio.sleep(0.01)
+                    pyautogui.click(219,21)
+                    await asyncio.sleep(0.05)
+                # elif self.tarkov_is_active and self.ensure_inventory_close and self.inventory_is_open:
+                #     pyautogui.press('tab')
+                
+            last = time.time()
+                
 
-
-
-# main
-if __name__ == "__main__":
-    
-    event_loop = asyncio.new_event_loop()
-    event_loop2 = asyncio.new_event_loop()
-    # STart executor task, this is the main loop
-    active = True
-    executor_task = event_loop.create_task(action_executor_task())
-    
-    
-    a = SingleAction(press_key, ["w", 1])
-    event_loop.run_forever()
+                        
+    def start(self):
+        f1 = asyncio.ensure_future(self.main_loop())
+        f2 = asyncio.ensure_future(self.check_tarkov_task())
+        f3 = asyncio.ensure_future(self.check_inv_task())
+        f4 = asyncio.ensure_future(self.check_in_raid_task())
+        return [f1, f2, f3, f4]
+                
+    def disable(self):
+        self.active = False
         
-    # Wait for task to finish
-    event_loop2.run_until_complete(queue_action(a))
+    def enable(self):
+        self.active = True
+        
+    def stop(self):
+        self.running = False
+
+
+
+if __name__ == '__main__':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    bust = Buster()
     
-    sleep(5)
-    running = False
-    # Wait for executor_task to finih, dont use await
-    pass
+    tasks = bust.start()
+    
+
+    a = SingleAction(bust.drop_all_wearable, [], bust)
+    # comp = CompoundAction([a, b], bust)
+    bust.active = True
+    bust.tarkov_is_active = True
+        
+        
+    import threading
+    # Thread that waits for user input, calls bust.stop() when user presses enter
+    def wait_for_input():
+        input("Say when.")
+        bust.stop()
+    threading.Thread(target=wait_for_input).start()
+
+    
+    asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
+    
+    # main()
---
+++ README.md
@@ -0,0 +1,12 @@
+# Instructions:
+
+1. UnZip `BitBust.zip`
+2. Run `Bitty.exe` once (It will flash)
+    * (This will exit and create a 'prices.json')
+3. Edit "prices.json" with desired bit prices 
+    * (0 to disable)
+4. Start `Bitty.exe` and authenticate it with Twitch 
+    * This only asks for read permissions in chat
+5. Suffer
+    * Hit `ENTER` twice to exit the program
+
